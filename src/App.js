aa4c9714dae4d97de5daa11cb7944cf3ef0d4334
Sebastian Augustyn <spaugustyn@gmail.com>
2024-02-08 22:08:56 +0100

    Fix Navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import HomePage from './pages/HomePage/HomePage';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
